a247c282658fe091d883aa02bc262886c62e20c1
Kamil2601 <mich.kamil2601@gmail.com>
2020-04-09 14:25:49 +0200

    POO lista 4

---
+++ Semestr_6/Projektowanie_Obiektowe_Oprogramowania/Lista_4/Zad1/SingletonC/SingletonC.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class SingletonC
+{
+    private static SingletonC instance;
+    private static DateTime creationTime;
+    private static Object _lock = new Object();
+    private SingletonC()
+    {
+        creationTime = DateTime.Now;
+    }
+    private static bool EnoughTimePassed()
+    {
+        return DateTime.Now > creationTime.AddSeconds(5);
+    }
+    public static SingletonC Instance()
+    {
+        if (instance == null || EnoughTimePassed())
+        {
+            lock (_lock)
+            {
+                if (instance == null || EnoughTimePassed())
+                {
+                    instance = new SingletonC();
+                }
+            }
+        }
+        return instance;
+    }
+};
---
+++ Semestr_6/Projektowanie_Obiektowe_Oprogramowania/Lista_4/Zad2/Factory/ShapeFactory.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Factory
+{
+    public interface IShape{
+        int getWidth();
+        int getHeight();
+    }
+
+    public interface IShapeFactoryWorker{
+        bool AcceptParameters(string name, params object[] parameters);
+        IShape Create(params object[] parameters);
+    }
+
+    public class Square : IShape{
+        public int size;
+
+        public Square(int size)
+        {
+            this.size = size;
+        }
+
+        public int getHeight()
+        {
+            return this.size;
+        }
+
+        public int getWidth()
+        {
+            return this.size;
+        }
+    }
+
+    public class SquareFactoryWorker : IShapeFactoryWorker{
+
+        public bool AcceptParameters(string name, params object[] parameters)
+        {
+            return name == "Square" && parameters.Length==1;
+        }
+
+        public IShape Create(params object[] parameters)
+        {
+            return new Square((int)parameters[0]);
+        }
+    }
+
+    public class ShapeFactory
+    {
+        private List<IShapeFactoryWorker> workers = new List<IShapeFactoryWorker>();
+
+        public ShapeFactory()
+        {
+            RegisterWorker(new SquareFactoryWorker());
+        }
+
+        public void RegisterWorker(IShapeFactoryWorker worker)
+        {
+            workers.Add(worker);
+        }
+
+        public IShape CreateShape(string shapeName, params object[] parameters)
+        {
+            foreach (IShapeFactoryWorker worker in workers)
+            {
+                if (worker.AcceptParameters(shapeName,parameters))
+                {
+                    return worker.Create(parameters);
+                }
+            }
+
+            throw new ArgumentNullException();
+        }
+    }
+}
