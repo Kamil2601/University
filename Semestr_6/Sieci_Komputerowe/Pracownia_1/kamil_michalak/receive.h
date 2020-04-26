4ad7e7a5ad2fa6ae0f43b84a3057fd2ca90ac593
Kamil2601 <mich.kamil2601@gmail.com>
2020-04-26 20:23:22 +0200

    sieci

--- Semestr_6/Sieci_Komputerowe/Pracownia_1/kamil_michalak/receive.h
+++
@@ -1,5 +0,0 @@
-// Kamil Michalak 299089
-#include "includes.h"
-#include <set>
-
-bool receive_icmp(int& sockfd, int ttl);
---
+++ Semestr_6/Sieci_Komputerowe/Pracownia_2/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+struct Interface
+{
+    string ip;
+    int distance;
+};
+
+
+int main()
+{
+    string xd;
+
+    cin >> xd;
+
+    cout << xd << '\n';
+
+    return 0;
+}
