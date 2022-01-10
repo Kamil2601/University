270e217f14a302d2c98b45a49eb54f0222040710
Kamil2601 <mich.kamil2601@gmail.com>
2022-01-10 19:59:28 +0100

    ML Assignment 2.3 DT task 3

--- Studia_magisterskie/Semestr_2/Zaawansowany_C++/Lista_9/Zad5.cpp
+++ Studia_magisterskie/Semestr_2/Zaawansowany_C++/Lista_9/Zad5.cpp
@@ -13,18 +13,22 @@ std::string get_href(std::string a)
 
     auto href = href_match.str();
 
-    return href.substr(6, href.size()-7);
+    if (href.size() > 6)
+        return href.substr(6, href.size()-7);
+    else return "";
 }
 
 void print_refs(std::string text)
 {
-    const static std::regex reg("<a[^>]+>");
+    const static std::regex reg("<a[^>]+href=\"[^>]+>");
 
     std::smatch a_match;
 
     while (std::regex_search(text, a_match, reg))
     {
-        std::cout << get_href(a_match.str()) << "\n";
+        auto link = get_href(a_match.str());
+
+        std::cout << link << "\n";
 
         text = a_match.suffix();
     }
@@ -35,24 +39,24 @@ int main(int argc, char* argv[])
     // std::string html = "<p> <a href=\"www.facebook.com\"><a href=\"www.google.com\"></a>";
 
 
-    std::string x = "<a name=\"abc\">";
-    print_refs(x);
+    // std::string x = "<a href=\"abc\">xdddd</a> <a name=\"xd\"></a>    <a href=\"test\"></a>";
+    // print_refs(html);
 
 
     // std::cout << argv[1] << "\n";
 
-    // std::ifstream text_file;
+    std::ifstream text_file;
 
-    // text_file.open(argv[1]);
+    text_file.open(argv[1]);
 
-    // std::string line;
+    std::string line;
 
-    // // std::string text = text_file.read
+    std::string text;
 
-    // while (std::getline(text_file, line))
-    // {
-    //     // std::cout << line << "\n";
-    //     if (line.size() > 0)
-    //         print_refs(line);
-    // }
+    while (std::getline(text_file, line))
+    {
+        text += line;
+    }
+
+    print_refs(text);
 }
